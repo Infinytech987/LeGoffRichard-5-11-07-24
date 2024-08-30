7fc3eb511973feb27a70e7101eed9a04cdbe48ec
InfinyTech <contact@infinytech.io>
2024-08-30 12:36:01 +0200

    commit 30/08

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,21 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+
+console.log(slides);
+
+const arrow_left = document.querySelector(".arrow_left");
+
+const arrow_right = document.querySelector(".arrow_right");
+
+const img = document.querySelector("banner-img");
+
+console.log(arrow_left, arrow_right);
+console.log(img);
+
+arrow_left.addEventListener("click", function () {
+  alert("Bouton cliqué !");
+});
+arrow_right.addEventListener("click", function () {
+  alert("Bouton cliqué !");
+});
